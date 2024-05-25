f1c77779b61e3208e8288684ee927acfc2e1c62c
Tiago Melo <Tiago.Melo@br.ey.com>
2024-05-24 23:42:19 -0300

    Add project files.

---
+++ DBTools/GET_I200xI250.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBTools
+{
+	internal class GET_I200xI250
+	{
+		public void ExecuteSelect(string connectionString)
+		{
+			try
+			{
+				using (SqlConnection connection = new(connectionString))
+				{
+					connection.Open();
+
+					CreateDynamicTable(connection);
+
+					connection.Close();
+				}
+
+				Console.WriteLine($"Concluído: {csvFilePath}");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Erro ao importar o arquivo: {csvFilePath} Mensagem: {ex.Message}");
+			}
+		}
+
+	}
+}
---
+++ DBTools/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DBConsole
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string connectionString = "Data Source=10.22.0.212\\INST1;Initial Catalog=DBEYCLIENT_33040981000150;Integrated Security=True;Connect Timeout=0;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+			GET_I200xI250 get = new();
+
+			get.ExecuteSelect(connectionString);
+
+			Console.ReadLine();
+		}
+	}
+}
